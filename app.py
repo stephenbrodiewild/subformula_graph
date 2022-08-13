fc5a59550bf7cd6109f84c335c1ed7ca6ed4e8bb
stephenbrodiewild <stephenbrodiewild@gmail.com>
2022-08-13 11:14:27 +0800

    Mass spectrum graph init

--- app.py
+++ app.py
@@ -34,20 +34,72 @@ app.layout = html.Div([
             'margin': '10px'
         }
     ),
-    dcc.Graph(id='chromatogram')
+    dcc.Graph(id='chromatogram'),
+    dcc.Graph(id='mass-spectrum')
 ])
 
+chromatogram = None
+
+
+def string_to_chromatogram(contents):
+    global chromatogram
+    content_type, content_string = contents.split(',')
+    try:
+        with tempfile.NamedTemporaryFile(mode='w+b', suffix=".CDF") as f:
+            f.write(base64.b64decode(content_string))
+            f.seek(0)
+            chromatogram = Chromatogram(f.name)
+    except Exception as e:
+        print(e)
+
+
+
+@app.callback(Output('mass-spectrum', 'figure'),
+              Input('chromatogram', 'clickData'),
+              prevent_initial_call=True)
+def update_mass_spectrum(click):
+    """
+{
+   "points":[
+      {
+         "curveNumber":0,
+         "pointNumber":1203,
+         "pointIndex":1203,
+         "x":13.488432884216309,
+         "y":422233.84375,
+         "bbox":{
+            "x0":715.83,
+            "x1":717.83,
+            "y0":245.05,
+            "y1":247.05
+         }
+      }
+   ]
+}
+    """
+    if click:
+        scan_number = int(click['points'][0]['x'])
+        mass_spectrum = chromatogram.get_raw_MS(scan_number)
+        max_mass = int(max(mass_spectrum.masses)) + 1
+        ms_info = mass_spectrum.get_MS()
+        ms_info_rounded = {int(round(m,0)): ms_info[m] for m in ms_info}
+        mz = [i for i in range(max_mass)]
+        a = [ms_info_rounded[i] if i in ms_info_rounded else 0 for i in range(max_mass)]
+
+        #fig = px.bar(x=mz, y=a, title='Mass Spectrum', color='#6666ff')
+        df = pd.DataFrame(data={'mz': mz, 'a': a})
+        fig = px.bar(df, x='mz', y='a', title='Mass Spectrum')
+        fig.update_layout(transition_duration=500)
+        return fig
+
 @app.callback(Output('chromatogram', 'figure'),
-              Input('upload-data', 'contents'), prevent_initial_call=True)
+              Input('upload-data', 'contents'),
+              prevent_initial_call=True)
 def update_chromatogram(contents):
     if contents is not None:
-        content_type, content_string = contents.split(',')
         try:
-            with tempfile.NamedTemporaryFile(mode='w+b', suffix=".CDF") as f:
-                f.write(base64.b64decode(content_string))
-                f.seek(0)
-                print(f.name)
-                chromatogram = Chromatogram(f.name)
+            string_to_chromatogram(contents)
+            if(chromatogram):
                 df = pd.DataFrame({'time': [chromatogram.times[i]/60 for i in range(
                     chromatogram.maxscan)], 'Intensity': chromatogram.ion_current})
                 fig = px.line(df, x='time', y='Intensity')
@@ -59,4 +111,4 @@ def update_chromatogram(contents):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8000)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
